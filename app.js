4e401f24fa1a74f0f07b7c54e18cf9c25a6b6d91
Mathieu Gosbee <mail@matbee.com>
2013-12-05 01:36:49 -0500

    .

--- app.js
+++ app.js
@@ -116,22 +116,23 @@ wh.on("click", function (victim, clicker) {
 wh.on("battle", function (channel, myClientId) {
   myClientId = myClientId || this.socket.id
   var self = this;
-  writeClient.sadd("battle:"+channel, JSON.stringify({myClientId: myClientId, socketId: this.socket.id}), function () {
+  writeClient.sadd("battle:"+channel, JSON.stringify({clientId: myClientId, socketId: this.socket.id}), function () {
     readClient.smembers("battle:"+channel, function (err, members) {
       if (!err && members && members.length > 0) {
         members.forEach(function (member) {
           member = JSON.parse(member);
           if (member != "undefined") {
-            self.rpc.userJoined(null, member.myClientId, member.socketId);
+            self.rpc.userJoined(null, member.clientId, member.socketId);
           }
         });
       }
     });
   });
-  redisSub.publish("battle:"+channel, myClientId, function(){});
-  var battleChannelCB = function (id) {
-    if (id != myClientId) {
-      self.rpc.userJoined(null, id);
+  redisSub.publish("battle:"+channel, JSON.stringify({clientId: myClientId, socketId: this.socket.id}), function(){});
+  var battleChannelCB = function (data) {
+    data = JSON.parse(data);
+    if (data.clientId != myClientId) {
+      self.rpc.userJoined(null, data.clientId, data.socketId);
     }
   };
   redisSub.on("battle:"+channel, battleChannelCB);
